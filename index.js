7427d3181ff768ffdb8b8bceee56579946c5cffa
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-09-13 15:24:59 +0200

    beautify the code

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }))
 app.set('view engine', 'handlebars');
-app.use(session({secret:'keyboard cat', cookie: {maxAge:60000}}))
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: {
+    maxAge: 60000
+  }
+}))
 app.use(cookieParser('keyboard cat'));
 app.use(flash());
 app.use(bodyParser.urlencoded({
@@ -24,18 +29,18 @@ app.use(express.static('public'));
 app.get('/waiters/:username', function(req, res, next) {
   var waiterName = req.params.username;
   //var waiterName = waiterName.substring(0, 1).toUpperCase() + waiterName.substring(1);
-  //  console.log(waiterName);
+
   // get the user in the database for the waiterName entered
   models.Shifts.findOne({
-    name : waiterName
+    name: waiterName
 
-  }, function(err, waiter){
-    if(err){
+  }, function(err, waiter) {
+    if (err) {
       return next(err);
     }
 
     res.render('index', {
-      waiters:'Hi, ' + waiterName + " please select your days",
+      waiters: 'Hi, ' + waiterName + " please select your days",
       waiter: waiter
     })
   })
@@ -48,8 +53,8 @@ app.post('/waiters/:username', function(req, res) {
   // console.log(days);
   var waiterName = req.params.username;
   myShift = {}
-  if(!days){
-    req.flash('submitmsg','please select atleast one day!')
+  if (!days) {
+    req.flash('submitmsg', 'please select atleast one day!')
     res.redirect('/waiters/' + waiterName)
     return
   }
@@ -76,7 +81,7 @@ app.post('/waiters/:username', function(req, res) {
             name: waiterName,
             days: myShift
           })
-          shiftDays.save(function(err,data) {
+          shiftDays.save(function(err, data) {
             if (err) {
               console.log(err);
             } else {
@@ -87,143 +92,102 @@ app.post('/waiters/:username', function(req, res) {
             }
           });
 
-        }
-        else{
-          // console.log(data.days.Monday);
-          // res.render('index',{
-          // submitmsg:  "Days updated successfully!",
-          // days:data
-          // })
+        } else {
+
           req.flash('submitmsg', "your days have been updated successfully")
-          res.redirect('/waiters/'+ waiterName)
+          res.redirect('/waiters/' + waiterName)
         }
       }
     })
 });
 
 
- app.get('/days',function(req, res){
+app.get('/days', function(req, res) {
 
-   var waitersRoster = {
-     Monday : {
-       waiters: []
-     },
-     Tuesday : {
-       waiters: []
-     },
-     Wednesday : {
-       waiters: []
-     },
-     Thursday : {
-       waiters: []
-     },
-     Friday : {
-       waiters: []
-     },
-     Saturday : {
-       waiters: []
-     },
-     Sunday : {
-       waiters: []
-     }
-   }
+  var waitersRoster = {
+    Monday: {
+      waiters: []
+    },
+    Tuesday: {
+      waiters: []
+    },
+    Wednesday: {
+      waiters: []
+    },
+    Thursday: {
+      waiters: []
+    },
+    Friday: {
+      waiters: []
+    },
+    Saturday: {
+      waiters: []
+    },
+    Sunday: {
+      waiters: []
+    }
+  }
 
-    models.Shifts.find({},function(err, results){
-    if(err){
-       console.log(err);
-     }
-     else{
-       var weekDays = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
-       results.forEach(function(waiterShift){
-         weekDays.forEach(function(day){
-           if(waiterShift.days[day]){
-            //  console.log("+++++++++++++++" + day);
-             waitersRoster[day].waiters.push(waiterShift.name)
-           }
+  models.Shifts.find({}, function(err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+      results.forEach(function(waiterShift) {
+        weekDays.forEach(function(day) {
+          if (waiterShift.days[day]) {
+          waitersRoster[day].waiters.push(waiterShift.name)
+          }
 
-         })
-       })
-     }
+        })
+      })
+    }
 
 
 
-     res.render('admin',{
-       waitersADay:waitersRoster,
-       monDisplay:ColourManaging(waitersRoster.Monday.waiters.length),
-       tueDisplay:ColourManaging(waitersRoster.Tuesday.waiters.length),
-       wedDisplay:ColourManaging(waitersRoster.Wednesday.waiters.length),
-       thuDisplay:ColourManaging(waitersRoster.Thursday.waiters.length),
-       friDisplay:ColourManaging(waitersRoster.Friday.waiters.length),
-       satDisplay:ColourManaging(waitersRoster.Saturday.waiters.length),
-       sunDisplay:ColourManaging(waitersRoster.Sunday.waiters.length)
-     })
-   });
- });
+    res.render('admin', {
+      waitersADay: waitersRoster,
+      monDisplay: ColourManaging(waitersRoster.Monday.waiters.length),
+      tueDisplay: ColourManaging(waitersRoster.Tuesday.waiters.length),
+      wedDisplay: ColourManaging(waitersRoster.Wednesday.waiters.length),
+      thuDisplay: ColourManaging(waitersRoster.Thursday.waiters.length),
+      friDisplay: ColourManaging(waitersRoster.Friday.waiters.length),
+      satDisplay: ColourManaging(waitersRoster.Saturday.waiters.length),
+      sunDisplay: ColourManaging(waitersRoster.Sunday.waiters.length)
+    })
+  });
+});
 
 
 
- function ColourManaging(color){
-   if(color === 3){
-     return 'colour1';
-   }
-   else if(color < 3){
-     return 'colour3';
-   }
-   else if(color > 3){
-     return 'colour2';
-   }
- }
+function ColourManaging(color) {
+  if (color === 3) {
+    return 'colour1';
+  } else if (color < 3) {
+    return 'colour3';
+  } else if (color > 3) {
+    return 'colour2';
+  }
+}
 
 
- app.post('/reset', function(req, res) {
-   models.Shifts.remove({}, function(err, remove) {
-     if (err) {
-       return err;
-     }
-     res.redirect('/days')
+app.post('/reset', function(req, res) {
+  models.Shifts.remove({}, function(err, remove) {
+    if (err) {
+      return err;
+    }
+    res.redirect('/days')
 
-   });
- });
+  });
+});
 
 
 
 
-  //   for(var i=0; i< results.length; i++){
-  //         // console.log("d"+ day);
-  //         console.log("fgfgffffhfhfh" +results[i].days);
-  //         var shiftObj = results[i].days[day];
-  //         console.log("+++++++++++" + shiftObj);
-  //  })
-    // for(var waiterDays in shiftObj ){
-      // if(shiftObj){
-      //   Monday.push(results[i].name)
-      // }
-      // if(shiftObj){
-      //   Tuesday.push(results[i].name)
-      // }
-      // if(shiftObj){
-      //   // console.log(results[i].name);
-      //   Wednesday.push(results[i].name)
-      // }
-      // if(shiftObj){
-      // Thursday.push(results[i].name)
-      // }
-      // if(shiftObj){
-      //   Friday.push(results[i].name)
-      // }
-      // if(shiftObj){
-      //   console.log("_________"+results[i].name);
-      //   Saturday.push(results[i].name)
-      // }
-      // if(shiftObj){
-      //   Sunday.push(results[i].name)
-      // }
-    // }
 
 
 
-
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.log(err.stack);
   res.send(err.stack);
 })
